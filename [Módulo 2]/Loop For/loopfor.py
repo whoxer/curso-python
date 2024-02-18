5b3ef4a93292835282930c4f74384a8812dcd62a
whoxer <natanaelvsant@gmail.com>
2024-02-17 22:06:30 -0300

    Arquivo controle de fluxo com o Loop For adicionado

---
+++ [Módulo 2]/Loop For/loopfor.py
@@ -0,0 +1,20 @@
+# Controle de Fluxo - Loop For
+
+# Lista criada com nomes de revolucionários na história da humanidade
+nomes_de_revolucionarios =  [
+    'Malcom X',
+    'Karl Marx',
+    'Amaro Félix',
+    'Angela Davis',
+    'Olga Benário',
+    'Alexandra Elbakyan',
+    'Jorge Amado'
+]
+
+print("Nomes de revolucionários na história da Humanidade: \n")
+
+
+# O loop for busca pelo nome dentro da lista que criamos
+for nome in nomes_de_revolucionarios:
+    # Após isso o comando print() exibe nome por nome da lista
+    print(nome, '\n')
