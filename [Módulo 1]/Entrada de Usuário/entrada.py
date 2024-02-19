d5a9ce717f7dbdccc0b92fe8adce9a3ec8fa34b6
whoxer <natanaelvsant@gmail.com>
2024-02-19 18:27:08 -0300

    Arquivo entrada.py explicando o input do usuario em python

---
+++ [Módulo 1]/Entrada de Usuário/entrada.py
@@ -0,0 +1,10 @@
+# Tipos de Dados - Entrada De Usuário
+
+# Inserção de nome de usuário
+nome = str(input("> Digite seu nome:\n> "))
+
+# Inserção de número inteiro
+idade = int(input("> Digite sua idade:\n> "))
+
+# Aqui podemos passar como parâmetro
+print("> Olá", nome, "você tem", idade, "anos!")
