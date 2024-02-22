9e33d988fd9ef9904e02a3079fc9e13d9eb5b453
whoxer <natanaelvsant@gmail.com>
2024-02-22 16:49:44 -0300

    Tipos de dados listas adicionado

--- [Módulo 1]/Listas/listas.py
+++ [Módulo 1]/Listas/listas.py
@@ -1,21 +1,45 @@
+# Tipos de Dados - Listas
+
+# Listas basicamente são um conjunto de elementos em sequência
+lista_numeros = [
+    8,
+    16,
+    32,
+    64,
+    128
+]
+
+# Aqui nós escrevemos toda a lista
+print (
+    "Lista de números: ",
+    lista_numeros
+)
 
 
-#Listas basicamente são um conjunto de elementos em sequencia
+# Aqui nós escrevemos apenas um dos números da lista
+print (
+    "Acessando o valor",
+    lista_numeros[2], # Dessa forma você acessa o valor
+    "do endereço",
+    lista_numeros.index(32) # E dessa forma o valor da posição em que o número '256' está
+)
+
+# Adicionando um novo elemento a lista
+
+lista_numeros.append(256) # Aqui colocamos o elemento 256 na lista veja:
+
+print (
+	"Lista de números atualizada com o 256: ",
+	lista_numeros
+)
+
+# Removendo o elemento criado da lista
+
+lista_numeros.remove(256) # Aqui removemos o 256 da lista de numeros veja:
+
+print (
+	"Lista de números atualizada sem o 256",
+	lista_numeros
+)
 
 
-lista_numeros = [1, 2, 3, 4, 5]
-
-# E como adiciono um novo numero na lista?
-
-lista_numeros.append(6) 
-
-# Se quiser acessar um numero dentro da lista
-
-lista_numeros[3] = 10
-
-# Você acessou o numero '3' dentro da lista
-# Removendo o segundo elemento da lista (índice 1)
-del lista_numeros[1]
-
-# Imprimindo a lista resultante
-print(lista_numeros)  # Saída: [1, 3, 10, 5, 6]
