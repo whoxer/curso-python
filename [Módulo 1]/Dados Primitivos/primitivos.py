91349c064a44c43dff913d644706e8395781e896
whoxer <natanaelvsant@gmail.com>
2024-02-22 16:40:52 -0300

    Arquivo sobre tipos primitivos adicionado

---
+++ [Módulo 1]/Dados Primitivos/primitivos.py
@@ -0,0 +1,26 @@
+# Tipos de Dados - Tipos Primitivos
+
+
+# Você pode escrever textos dentro de aspas com o python
+print ("Isso é um texto convencional!")
+
+
+
+# Mas melhor ainda passar textos para variáveis, por exemplo:
+
+viva = 'Viva ao socialismo!' # aspas simples
+olga_benario = "Lutei pelo justo, pelo bom, pelo melhor do mundo! - Olga Benário" # ou aspas duplas
+
+# Depois disso é so escrever dentro dos parênteses o nome da variável
+print (olga_benario)
+print (viva)
+
+# Você também é capaz de passar números também!
+
+rev_cubana = 1959
+numero_pi = 3.14159
+
+# Com o print você pode combinar os valores, veja a exemplo:
+
+print ("Ano da revolução cubana:", rev_cubana)
+print ('Valor de PI:', numero_pi)
